cebab532637048d18cdf54184a2d41ae42246a8e
omarino-go <moulomar9099@gmail.com>
2025-05-17 21:21:40 +0100

    Create sum_two_integers.c

---
+++ sum_two_integers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int a,b;
+  printf("Entrez le premiér nombre : ");
+  scanf("%d",&a);
+  printf("Entrez le deuxième nombre : ");
+  scanf("%d",&b);
+  int somme= a+b;
+  printf("La somme est : ",somme);
+  return 0;
+}
