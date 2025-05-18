e702bdb5a64f74b045adcb0d2c36bfe75073500a
omarino-go <moulomar9099@gmail.com>
2025-05-18 22:16:33 +0100

    Update sum_two_integers.c

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
 int main(){
   int a,b;
-  printf("Entrez le premiér nombre : ");
-  scanf("%d",&a);
-  printf("Entrez le deuxième nombre : ");
-  scanf("%d",&b);
+  scanf("%d %d",&a,&b);
   int somme= a+b;
-  printf("La somme est : ",somme);
+  printf("La somme est : %d",somme);
   return 0;
 }
