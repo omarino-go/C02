009a3e001b9f099a02ba6749b97a1a382c59962f
omarino-go <moulomar9099@gmail.com>
2025-05-18 22:24:54 +0100

    Create swap_no_temp.c

---
+++ swap_no_temp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int a,b;
+  //recuperation du variable
+  scanf("%d %d",&a,&b);
+  //echange sans variable temporaire
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  //affichage du resultat
+  printf("Après échange: a = %d, b = %d",a,b");
+}
