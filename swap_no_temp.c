ec4bf05a546f4892f77e68c162b7f148eb62e2d5
omarino-go <moulomar9099@gmail.com>
2025-05-19 23:30:52 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-#include<locale.h>
+#include <locale.h>
 int main(){
-  setlocale(LC_ALL,"")
+  setlocale(LC_ALL,"");
   int a,b;
   //recuperation du variable
   scanf("%d %d",&a,&b);
