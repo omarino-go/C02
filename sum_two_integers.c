3812bdba88e044e74e07fc5b6b67ad9839462c4b
omarino-go <moulomar9099@gmail.com>
2025-05-19 21:53:17 +0100

    Update sum_two_integers.c

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main(){
+  //variable
   int a,b;
+  //recupération des nombre
   scanf("%d %d",&a,&b);
+  //la variable qui contient la somme
   int somme= a+b;
+  //affiché la résultat
   printf("La somme est : %d",somme);
   return 0;
 }
