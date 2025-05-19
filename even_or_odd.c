c466b7eae413e36be8ee88de8c29b927c17d0131
omarino-go <moulomar9099@gmail.com>
2025-05-19 21:49:32 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 main(){
+  //Les variable
   int i,P;
   scanf("%d",&i);
+  //recuperation du reste du division par 2
   P=i%2;
+  //si reste du division par 2 est égale à 0 alors
   if (P==0){
       printf("%d est pair.",i);}
+  //sinon le nbre est impair
   else{
     printf("%d est impair.",i);}
 }
