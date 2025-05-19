aa64d4aacabdd92cad2854ebdee066fd37004028
omarino-go <moulomar9099@gmail.com>
2025-05-19 21:47:31 +0100

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 main(){
+  //variable
   int v=1,a,i;
   scanf("%d",&a);
+  //boucle pour multiplier plusièurs nbre
   for (i=a;i>=1;i--)
       v*=i;
   printf("Le factoriel de %d est : %d",a,v);
