d367069f05c583a330117ef4dbef88682bc4fa6c
omarino-go <moulomar9099@gmail.com>
2025-05-20 21:16:43 +0100

    Create swap_no_temp.c

---
+++ swap_no_temp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<locale.h>
+main(){
+  Setlocale(LC_ALL, "");
+  int a,b;
+  //recuperation du variable
+  scanf("%d %d",&a,&b);
+  //echange sans variable temporaire
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  //affichage du resultat
+  printf("Après échange: a = %d, b = %d",a,b);
+  return 0;
+}
