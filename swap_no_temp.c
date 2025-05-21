700014de1a8b4f659e0c87c04f0c16f8761ac656
omarino-go <moulomar9099@gmail.com>
2025-05-21 18:19:28 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -5,13 +5,12 @@ int main(){
   setlocale(LC_ALL, "");
   int a,b;
   //recuperation du variable
-  scanf("%d",&a);
-  scanf("%d",&b);
+  scanf("%d %d",&a,&b);
   //echange sans variable temporaire
   a=a+b;
   b=a-b;
   a=a-b;
   //affichage du resultat
-  printf("Après échange: a = %d, b = %d",a,b);
+  printf("Après échange: a = %d, b = %d ",a,b);
   return 0;
 }
