3fd353482a93af39d0a5b219f50156b25a7ae7e3
omarino-go <moulomar9099@gmail.com>
2025-05-23 23:21:43 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
-//bibliothèque qui gère les accents
-#include<locale.h>
 int main(){
-  setlocale(LC_ALL, "");
   int a,b;
   //recuperation du variable
-  scanf("%d %d",&a,&b);
+  scanf("%d %d", &a, &b);
   //echange sans variable temporaire
   a=a+b;
   b=a-b;
   a=a-b;
   //affichage du resultat
-  printf("\nAprès échange: a = %d, b = %d\n",a,b);
+  printf("Après échange: a = %d, b = %d\n", a, b);
   return 0;
 }
