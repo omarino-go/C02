3b449de30b4967e9ce965c88f77c24babcf6c816
omarino-go <moulomar9099@gmail.com>
2025-05-25 19:50:47 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,25 +1,13 @@
 #include<stdio.h>
-int main()
-{
-    int a,b;
-
-    scanf("%d", &a);
-    scanf("%d", &b);
-
-
-    a = a + b; 
-    // a = a + b                      (nouvelle valeur de a)
-
-    b = a - b; 
-    // b = a - b => b = a + b - b = a (nouvelle valeur de b)
-
-    a = a - b; 
-    // a = a - b => a = a + b - a = b (nouvelle valeur de a)
-
-    
-    int v1 = a, v2 = b;
-
-    
-    printf("Après échange : a = %d, b = %d", v1, v2);
-
+int main(){
+  int a,b;
+  //recuperation du variable
+  scanf("%d %d",&a,&b);
+  //echange sans variable temporaire
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  //affichage du resultat
+  printf("Après échange: a = %d, b = %d ",a,b);
+  return 0;
 }
